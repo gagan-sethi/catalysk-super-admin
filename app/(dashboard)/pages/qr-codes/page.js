76e97c7f486acd7ae815f78763052830b546b451
gagan-sethi <ravisethi7102@gmail.com>
2024-12-21 09:08:47 +0530

    Token fix on qr code

--- app/(dashboard)/pages/qr-codes/page.js
+++ app/(dashboard)/pages/qr-codes/page.js
@@ -8,6 +8,14 @@ const QRCodes = () => {
   const [showModal, setShowModal] = useState(false);
   const [newQrCodeFile, setNewQrCodeFile] = useState(null);
 
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    // Only runs on the client-side
+    const tokenFromLocalStorage = localStorage.getItem("token");
+    setToken(tokenFromLocalStorage || "");
+  }, []);
+
   const API_URL = "https://betazone.promaticstechnologies.com/admin/payment";
 
   const fetchQRCode = async () => {
@@ -15,7 +23,7 @@ const QRCodes = () => {
       const response = await axios.get(`${API_URL}/getQRCode`, {
         headers: {
           Authorization:
-            "Bearer d527c719af2db07b02b744f836bd3361b4609c45bade79e1b9417641f79022e8935ac128ed40cc8fb52279e56cfcfba86d2d86d40ea005fb6192bb3f906ee49fe984947f584fb0661785c49afc6553b4da9c2ad86c8a4ed07d100f370e8fc2343a74c3ed68d3fe2768612cde0b208ee5444f3b902a436dc4a5d6f900ceea866c33c83265b708c617cde2ac6dc755456a491236d8e996e3b8f740435459619c13282276d91505d74839aa129b0a17f16a4976c589b59944104ec6927ecc2fab3eddd67087a1aa5d4444462cd48be77a8d",
+            `Bearer ${token}`,
         },
       });
       setQrCodeData(response.data.data);
